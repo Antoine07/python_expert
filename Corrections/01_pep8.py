e286ff4aafa485e5dc3fdaf5e68c1d56f4e5eb56
Antoine07 <antoine.lucsko@wanadoo.fr>
2025-11-03 11:40:15 +0100

    correction exos

---
+++ Corrections/01_pep8.py
@@ -0,0 +1,15 @@
+# import this 
+
+def addNumbers(a, b):
+    return(a + b)
+
+nums=[1, 2, 3]
+for n in nums:
+    print(addNumbers(n, n))
+    
+res = []
+for number in numbers:
+    # print(number**2)
+    res.append(number**2)
+
+print(res)
---
+++ Corrections/02_listcomprehension.py
@@ -0,0 +1,6 @@
+notes = [10, 15, 8, 18, 13]
+
+res = [ n for n in notes if n >= 12 ]
+print(res)
+
+print([ n*2 for n in notes])
