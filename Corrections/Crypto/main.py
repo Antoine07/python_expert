9dee191decf2d1619929bfb9bf468555483a788e
Antoine07 <antoine.lucsko@wanadoo.fr>
2025-11-04 14:38:02 +0100

    example orga

--- Corrections/Crypto/main.py
+++ Corrections/Crypto/main.py
@@ -1,5 +1,8 @@
-# tous les scripts
+# tous les scripts dans main.py
 
+# Importer vos différentes classes dans ce fichier avec la syntaxe suivante
+# En Python un fichier est considérer comme un module que l'on peut importer dans
+# un autre fichier comme suit from le_fichier_sans_extention import ta_class, ta_fonction
 from Wallet import Wallet
 from Order import Order
 
